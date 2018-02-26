24a715a1a557403d8bd133e6924c0fb0f0c0010f
Tom Atkinson <thomas@atkinsons.me.uk>
2018-02-26 21:26:31 +0000

    fixed vm.conf and makefiles for -current vm.conf syntax changes

--- Makefile
+++ Makefile
@@ -11,6 +11,7 @@ vmm:
 	@echo ""
 	@cd /tmp
 	@ftp -V https://raw.githubusercontent.com/gonzalo-/vmm_setup/master/hostname.vether0
+    @ftp -V https://raw.githubusercontent.com/gonzalo-/vmm_setup/master/hostname.bridge0
 	@ftp -V https://raw.githubusercontent.com/gonzalo-/vmm_setup/master/dhcpd.conf
 	@ftp -V https://raw.githubusercontent.com/gonzalo-/vmm_setup/master/pf.conf
 	@ftp -V https://raw.githubusercontent.com/gonzalo-/vmm_setup/master/vm.conf
@@ -27,7 +28,7 @@ vmm:
 	@mkdir -p /VMs/sets/snapshots/amd64/
 	@mkdir -p /VMs/linux
 	@cd /VMs/linux && ftp -V https://nl.alpinelinux.org/alpine/v3.6/releases/x86_64/alpine-virt-3.6.0-x86_64.iso
-	@cd /VMs/sets/snapshots/amd64/ && ftp -V https://fastly.cdn.openbsd.org/pub/OpenBSD/snapshots/amd64/bsd{.mp,.rd}
+	@cd /VMs/sets/snapshots/amd64/ && ftp -V https://fastly.cdn.openbsd.org/pub/OpenBSD/snapshots/amd64/bsd{.mp,.rd} && ftp -V https://fastly.cdn.openbsd.org/pub/OpenBSD/snapshots/amd64/install62.fs
 	@echo ""
 	@echo "[+] Enabling vmd & setting dhcpd..."
 	@echo ""
--- vmm.sh
+++ vmm.sh
@@ -9,6 +9,7 @@ echo "[+] Downloading files from https://github.com/gonzalo-/vmm_setup"
 echo ""
 cd /tmp
 ftp -V https://raw.githubusercontent.com/gonzalo-/vmm_setup/master/hostname.vether0
+ftp -V https://raw.githubusercontent.com/gonzalo-/vmm_setup/master/hostname.bridge0
 ftp -V https://raw.githubusercontent.com/gonzalo-/vmm_setup/master/dhcpd.conf
 ftp -V https://raw.githubusercontent.com/gonzalo-/vmm_setup/master/pf.conf
 ftp -V https://raw.githubusercontent.com/gonzalo-/vmm_setup/master/vm.conf
@@ -16,16 +17,17 @@ ftp -V https://raw.githubusercontent.com/gonzalo-/vmm_setup/master/sysctl.conf
 echo ""
 echo "[+] Installing files..."
 echo ""
-install -m 0640 -g wheel -o root /tmp/hostname.vether0 /etc && \
+install -m 0640 -g wheel -o root /tmp/hostname.{vether0, bridge0} /etc && \
 install -m 0600 -g wheel -o root /tmp/{pf,sysctl,dhcpd,vm}.conf /etc && \
-rm /tmp/hostname.vether0 /tmp/*.conf >/dev/null
+rm /tmp/hostname.* /tmp/*.conf >/dev/null
 echo ""
 echo "[+] Downloading OpenBSD kernels and Alpine Linux iso..."
 echo ""
 mkdir -p /VMs/sets/snapshots/amd64/
 mkdir -p /VMs/linux
 cd /VMs/linux && ftp -V https://nl.alpinelinux.org/alpine/v3.6/releases/x86_64/alpine-virt-3.6.0-x86_64.iso
-cd /VMs/sets/snapshots/amd64/ && ftp -V https://fastly.cdn.openbsd.org/pub/OpenBSD/snapshots/amd64/bsd{.mp,.rd}
+cd /VMs/sets/snapshots/amd64/ && ftp -V https://fastly.cdn.openbsd.org/pub/OpenBSD/snapshots/amd64/bsd{.mp,.rd} && ftp -V https://fastly.cdn.openbsd.org/pub/OpenBSD/snapshots/amd64/install62.fs
+vmctl "/VMs/OpenBSD_Ports.img" -s 25G && vmctl "/VMs/Alpine_Linux.img" -s 25G
 echo ""
 echo "[+] Enabling vmd & setting dhcpd..."
 echo ""
