9d9f03f1777951642922bf6d8902123b47bccaa5
Tom Atkinson <thomas@atkinsons.me.uk>
2018-02-26 21:59:14 +0000

    brought vmm.sh up to date with Makefile

--- vmm.sh
+++ vmm.sh
@@ -17,18 +17,19 @@ ftp -V https://raw.githubusercontent.com/teatkin/vmm_setup/master/sysctl.conf
 echo ""
 echo "[+] Installing files..."
 echo ""
-install -m 0640 -g wheel -o root /tmp/hostname.{vether0, bridge0} /etc && \
+install -m 0640 -g wheel -o root /tmp/hostname.vether0 /etc && \
+install -m 0640 -g wheel -o root /tmp/hostname.bridge0 /etc/ && \
 install -m 0600 -g wheel -o root /tmp/{pf,sysctl,dhcpd,vm}.conf /etc && \
 rm /tmp/hostname.* /tmp/*.conf >/dev/null
 echo ""
 echo "[+] Downloading OpenBSD kernels and Alpine Linux iso..."
 echo ""
-mkdir -p /VMs/sets/snapshots/amd64/
-mkdir -p /VMs/linux
-cd /VMs/linux && ftp -V https://nl.alpinelinux.org/alpine/v3.6/releases/x86_64/alpine-virt-3.6.0-x86_64.iso
-cd /VMs/sets/snapshots/amd64/ && ftp -V https://fastly.cdn.openbsd.org/pub/OpenBSD/snapshots/amd64/bsd{.mp,.rd} 
+mkdir -p /home/VMs/sets/snapshots/amd64/
+mkdir -p /home/VMs/linux
+cd /home/VMs/linux && ftp -V https://nl.alpinelinux.org/alpine/v3.6/releases/x86_64/alpine-virt-3.6.0-x86_64.iso
+cd /home/VMs/sets/snapshots/amd64/ && ftp -V https://fastly.cdn.openbsd.org/pub/OpenBSD/snapshots/amd64/bsd{.mp,.rd} 
 ftp -V https://fastly.cdn.openbsd.org/pub/OpenBSD/snapshots/amd64/install62.fs
-vmctl "/VMs/OpenBSD_Ports.img" -s 25G && vmctl "/VMs/Alpine_Linux.img" -s 25G
+vmctl "/home/VMs/OpenBSD_Ports.img" -s 25G && vmctl "/home/VMs/Alpine_Linux.img" -s 25G
 echo ""
 echo "[+] Enabling vmd & setting dhcpd..."
 echo ""
